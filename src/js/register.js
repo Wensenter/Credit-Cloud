f09fe6150c68c5a30f1daf02af4de0542e445f92
Wensenter <18365206169@163.com>
2019-08-05 15:55:06 +0800

    添加多级联动

--- src/js/register.js
+++ src/js/register.js
@@ -1,16 +1,38 @@
-var doc = document,
-    $community = doc.getElementById('community'),
-    $school = doc.getElementById('school'),
-    $communityRadio = doc.getElementsByClassName('community')[0],
-    $shcoolRadio = doc.getElementsByClassName('school')[0],
-    $name = doc.getElementById('community')
+window.onload = function(){
+    var doc = document,
+        $community = doc.getElementById('community'),
+        $school = doc.getElementById('school'),
+        $communityRadio = doc.getElementsByClassName('community')[0],
+        $shcoolRadio = doc.getElementsByClassName('school')[0],
+        $name = doc.getElementById('community')
 
-    $community.addEventListener('click',function(e){
+    $community.addEventListener('click', function (e) {
         $communityRadio.classList.add('active');
         $shcoolRadio.classList.remove('active');
-    },);
+    });
 
     $school.addEventListener('click', function (e) {
         $shcoolRadio.classList.add('active');
         $communityRadio.classList.remove('active');
-    }, );
+    });
+
+    var numArr = ['测试1', '测试2', '测试3', '测试4', '测试5'];
+
+    var mobileSelect3 = new MobileSelect({
+        trigger: '#trigger3',
+        title: '所属社区',
+        wheels: [
+            { data: numArr },
+            { data: numArr },
+            { data: numArr }
+        ],
+        position: [0, 0, 0, 0, 0],
+        transitionEnd: function (indexArr, data) {
+            //console.log(data);
+        },
+        callback: function (indexArr, data) {
+            console.log(data);
+        }
+    });
+}
+
